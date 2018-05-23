261592bf4305420f550f97f1bafa38ee35000293
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-23 23:41:22 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      resources :cocktails, only: [:index, :create, :new, :show ]  do
+      resources :doses, only: [:create, :new, :destroy ]
+  end
 end

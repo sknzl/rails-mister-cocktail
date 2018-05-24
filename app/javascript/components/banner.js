f27b804b0250cad7841185812fe8ec2a456e5fde
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-24 20:55:51 +0100

    added type.js

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  new Typed('#banner-typed-text', {
+    strings: ["Make your own cocktails", "Discover cocktails"],
+    typeSpeed: 40,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
